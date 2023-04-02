b6698b40798eea41a9384d5a960acd51134b8fdf
HendriS <hendristorm@gmail.com>
2023-04-02 23:05:12 +0200

    added documentation to models.py file

--- my_official_site/my_app/models.py
+++ my_official_site/my_app/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
+    """
+    This is a database-driven component that stores a single
+    Event entry. Each attribute represents a database field.
+    """
     name = models.CharField(max_length=120)
     event_date = models.DateTimeField()
     venue = models.CharField(max_length=120)
@@ -11,4 +15,12 @@ class Event(models.Model):
 
 
 def __str__(self):
+    """
+    Method that allows us to convert an object to a string
+    representation.
+
+    :param self:
+
+    :return: String representation of the object Event.
+    """
     return self.name
