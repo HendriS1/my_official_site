31e9eb54fedc9ef96fd7a4e3efa6b937377f7972
HendriS <hendristorm@gmail.com>
2023-04-03 19:55:54 +0200

    modified conf.py file

--- my_official_site/docs/conf.py
+++ my_official_site/docs/conf.py
@@ -10,7 +10,7 @@ import sys
 import django
 
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'my_app.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'my_official_site.settings'
 django.setup()
 
 project = 'django_app'
